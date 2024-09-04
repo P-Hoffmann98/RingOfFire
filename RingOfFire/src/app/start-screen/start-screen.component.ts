c4e9f64e537d58d4207693f7ff861d3cb4ce0a33
P-Hoffmann98 <patrick.hoffmann1998@yahoo.de>
2024-09-04 14:21:14 +0200

    start screen finished for now started with game

--- RingOfFire/src/app/start-screen/start-screen.component.ts
+++ RingOfFire/src/app/start-screen/start-screen.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
   imports: [],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
-export class StartScreenComponent {
+export class StartScreenComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
