d6364d1d5f43be68b475d89513223c25b68f34fc
P-Hoffmann98 <patrick.hoffmann1998@yahoo.de>
2024-09-04 16:23:10 +0200

    card draw animation and cardstacks

--- RingOfFire/src/app/game/game.component.ts
+++ RingOfFire/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,9 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.cardStack.pop() || '';
+      this.pickCardAnimation = true;
+
+      console.log(this.currentCard);
+      console.log(this.game);
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
---
+++ RingOfFire/src/models/game.ts
@@ -0,0 +1,33 @@
+export class Game {
+  public players: string[] = [];
+  public cardStack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayers: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.cardStack.push('spade_' + i);
+      this.cardStack.push('hearts_' + i);
+      this.cardStack.push('diamonds_' + i);
+      this.cardStack.push('clubs_' + i);
+    }
+    shuffle(this.cardStack);
+  }
+}
+
+function shuffle(array: string[]): void {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
